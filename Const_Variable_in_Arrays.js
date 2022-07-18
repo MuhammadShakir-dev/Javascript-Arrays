ace987d34a68d487d0b00b0efe030f178ce8f0bf
Muhammad Shakir <84896803+MuhammadShakir-dev@users.noreply.github.com>
2022-07-18 12:27:19 +0500

    Create Const_Variable_in_Arrays.js

---
+++ Const_Variable_in_Arrays.js
@@ -0,0 +1,12 @@
+ <script>
+      /*
+
+                 Constant varibale in arrays and objects
+
+
+*/
+      const a = ["Muhammad", "Shakir", "Dev"];
+      document.write(a + " <br> <br>");
+      a[2] = 49;
+      document.write(a + "<br> <br>");
+    </script>
